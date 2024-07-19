6b620c03e3d7752cfb7f528ef6e03725b77b92bc
Jason <ya850805@gmail.com>
2024-07-19 13:23:20 +0800

    feat: Add doOnXxx API demo.

--- chapter02-reactor/src/main/java/com/learning/reactor/FluxDemo.java
+++ chapter02-reactor/src/main/java/com/learning/reactor/FluxDemo.java
@@ -1,7 +1,10 @@
 package com.learning.reactor;
 
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.core.publisher.SignalType;
 
 import java.io.IOException;
 import java.time.Duration;
@@ -13,11 +16,112 @@ import java.time.Duration;
  *
  *  Mono：0|1個元素的流
  *  Flux：N個元素的流
+ *
+ *  響應式編成核心：看懂文檔的圖
+ *      信號：正常/異常(取消)  --> SignalType類
+ *          SUBSCRIBE：被訂閱
+ *          REQUEST：請求了N個元素
+ *          CANCEL：流被取消
+ *          ON_SUBSCRIBE：在訂閱的時候
+ *          ON_NEXT：在元素到達
+ *          ON_ERROR：在流錯誤
+ *          ON_COMPLETE：在流正常完成時
+ *          AFTER_TERMINATE：中斷以後
+ *          CURRENT_CONTEXT：當前上下文
+ *          ON_CONTEXT：感知上下文
+ *
+ *  doOnXxx API觸發時機：
+ *      1. doOnNext：每個數據(流的數據)到達的時候觸發
+ *      2. doOnEach：每個元素(流的數據和信號)到達的時候觸發
+ *      3. doOnRequest：消費者請求流元素的時候
+ *      4. doOnError：流發生錯誤
+ *      5. doOnSubscribe：流被訂閱的時候
+ *      6. doOnTerminate：發送取消/異常信號中斷了流
+ *      7. doOnCancel：流被取消
+ *      8. doOnDiscard：流中元素被忽略的時候
  **/
 public class FluxDemo {
     public static void main(String[] args) {
-        Mono<Integer> just = Mono.just(1);
-        just.subscribe(System.out::println);
+        // doOnNext：表示流中某個元素到達以後觸發一個回調
+        // 重要：doOnXxx要感知某個流的事件，就寫在這個流的後面、新流的前面
+        Flux.just(1, 2, 3, 4, 5, 6, 7, 0, 5, 6)
+                .doOnNext(i -> System.out.println("元素到達1：" + i))  // 元素到達時候觸發
+                .doOnEach(integerSignal -> {  // doOnEach封裝的更詳細
+                    System.out.println("doOnEach..." + integerSignal);
+                })
+                .map(i -> 10 / i)
+                .doOnError(throwable -> {
+                    System.out.println("異常：" + throwable);
+                })
+                .doOnNext(i -> System.out.println("元素到達2：" + i))
+                .subscribe(System.out::println);
+    }
+
+    public static void fluxDoOnXxx(String[] args) throws IOException, InterruptedException {
+//        Mono<Integer> just = Mono.just(1);
+//        just.subscribe(System.out::println);
+
+        // 事件感知：當流發生什麼事的時候，觸發一個回調，系統調用提前定義好的Hook鉤子函數：doOnXxx
+        // 鏈式API中，下面的操作符，操作的是上面的流
+        Flux<Integer> flux = Flux.range(1, 7)
+                .delayElements(Duration.ofSeconds(1))
+                .doOnComplete(() -> {
+                    System.out.println("流正常結束");
+                })
+                .doOnCancel(() -> {
+                    System.out.println("流已被取消");
+                })
+                .doOnError(throwable -> {
+                    System.out.println("流出錯了：" + throwable);
+                })
+                .doOnNext(integer -> {
+                    System.out.println("doOnNext：" + integer);
+                });
+
+        flux.subscribe(new BaseSubscriber<Integer>() {
+            @Override
+            protected void hookOnSubscribe(Subscription subscription) {
+                System.out.println("訂閱者和發布者綁定好了：" + subscription);
+                request(1);  // 要一個元素
+            }
+
+            @Override
+            protected void hookOnNext(Integer value) {
+                System.out.println("元素到達：" + value);
+                if (value < 5) {
+                    request(1);  // 繼續要元素
+                    if (value == 3) {
+                        throw new RuntimeException("ERROR....");
+                    }
+                } else {
+                    cancel();  // 取消訂閱
+                }
+            }
+
+            @Override
+            protected void hookOnComplete() {
+                System.out.println("數據流結束");
+            }
+
+            @Override
+            protected void hookOnError(Throwable throwable) {
+                System.out.println("數據流異常");
+            }
+
+            @Override
+            protected void hookOnCancel() {
+                System.out.println("數據流被取消");
+            }
+
+            @Override
+            protected void hookFinally(SignalType type) {
+                // 無論正常、異常都會執行
+                System.out.println("結束信號：" + type);
+            }
+        });
+//        Thread.sleep(2000);
+
+        System.in.read();
     }
 
     /**
